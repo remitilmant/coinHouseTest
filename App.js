7cee1ae08713e17e7eef3dd6f249d36e4f02cc53
Remi Conserto <remi.tilmant@conserto.pro>
2022-09-27 08:53:06 +0200

    App : init navigation

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import { QRScan } from "./Screens/QRScan";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 export default function App() {
   const Tab = createBottomTabNavigator();
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="LookUp" component={LookUp} />
-        <Tab.Screen name="QRScan" component={QRScan} />
+        <Tab.Group>
+          <Tab.Screen name="LookUp" component={LookUp} />
+          <Tab.Screen name="QRScan" component={QRScan} />
+        </Tab.Group>
       </Tab.Navigator>
     </NavigationContainer>
   );
