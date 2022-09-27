5bdc241fedf45c31f6ebb15db13414ff1547bd61
Remi Conserto <remi.tilmant@conserto.pro>
2022-09-27 14:09:28 +0200

    QRScan: Update wording

--- Screens/QRScan.jsx
+++ Screens/QRScan.jsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export function QRScan() {
   return (
     <View>
-      <Text>QRScan Page</Text>
+      <Text>Feature in development</Text>
     </View>
   );
 }
