533f91342b890af429de1443daec9655f0cf0a6f
Remi Conserto <remi.tilmant@conserto.pro>
2022-09-27 11:13:52 +0200

    App: add styles to tab navigator

--- App.js
+++ App.js
@@ -8,7 +8,16 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "black" },
+          tabBarStyle: { backgroundColor: "black", fontSize: 20 },
+          tabBarIconStyle: { display: "none" },
+          tabBarActiveTintColor: "white",
+          headerTitleStyle: { color: "white" },
+          headerTitle: "Coinhouse Etherscan",
+        }}
+      >
         <Tab.Group>
           <Tab.Screen name="LookUp" component={LookUp} />
           <Tab.Screen name="QRScan" component={QRScan} />
